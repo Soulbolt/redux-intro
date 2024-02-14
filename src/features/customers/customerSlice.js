a3726d2e77f10e5603b150194ffc5ca614b07d19
Soulbolt <soulbolt@gmail.com>
2024-02-13 18:43:23 -0700

    Add Features, accounts, customers folders. move  account and customer files to their respective folders. Add accountSlice and customerSlice files. Move account initialState, reducer, functions to accounts folder. and customers code accountSlice and customerSlice respectively.

---
+++ src/features/customers/customerSlice.js
@@ -0,0 +1,33 @@
+const initialStateCustomer = {
+  // account: initialStateAccount,
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/create":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/update":
+      return { ...state, fullName: action.payload.fullName };
+    default:
+      return state;
+  }
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/create",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
+}
